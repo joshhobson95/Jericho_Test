407eff0cadac18cc70be91f9409d0179b42c8bbd
Josh Hobson <joshuahobson1995@gmail.com>
2023-03-14 17:05:51 -0600

    end day one

---
+++ src/components/Banner.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Banner.css'
+
+
+function Banner() {
+  return (
+    <div className='banner'>
+        <div className='b_img'>
+        <div className='b_1'>
+            <div className='b_2'>
+            <h4 className='b_h4'>Let us help you make you yard and Oasis in the SouthWest</h4>
+            <h1>Spring has Sprung</h1>
+            <h4 className='b_h4g'> See what's come in this week</h4>
+            </div>
+            <button>Browse Now</button>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Banner
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,29 @@
 import React from 'react'
 import './Home.css'
+import Banner from './Banner'
+import Container from './Container'
 
 function Home() {
   return (
     <div className='home'>
 
       <div className='welcome'>
- 
-
 <div className='w'>
         <h4 className='w_1'>Welcome to</h4>
         <h1 className='w_2'>Jericho Nursery</h1>
         <h4 className='w_3'>All you Yard and Garden needs</h4>
 </div>
-
+<div className='p_1'>
+  <p>
+  Welcome to Jericho Nursery, our funky little garden center in Albuquerque’s North Valley. Jericho has its own vibe, we’re a little rustic and a pretty old school, but we’re savvy. We’ve got the tried-and-true products that keep the garden happy and healthy, as well as some great trendy items to pique the interest of new gardeners, and above all, the knowledge to point you in the right direction. We feel strongly that taking care of our people allows them to take better care of you, our customer, and our community. We believe that gardening is an evolutionary process with lots of trail and error, and we’re here to pull you up by your bootstraps when things go wrong and cheer you on in times of bounty. Your success is our success.
+  </p>
+</div>
       </div>
 
 
+
+    <Banner />
+  
 
 
 
